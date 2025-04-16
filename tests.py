871e924a65bc584d6660294354b84d2092c412b6
Christopher M Tran <ctran751@g.ucla.edu>
2025-04-15 17:33:22 -0700

    modify test3 by making it false and shorten length

--- tests.py
+++ tests.py
@@ -24,7 +24,7 @@ class CreditCardValidatorTest(unittest.TestCase):
             """
             Verifies if Master Cards with valid prefix (2221-2720) valid length and valid check bits passes .
             """
-            self.assertFalse(credit_card_validator("2221133742369166"))  
+            self.assertFalse(credit_card_validator("22210"))  
             
     def test4(self):
             """
